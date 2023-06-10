7311937e0dc1f1037f725d1d1f9806ebdfb25965
toshi472808 <to43.bz472808@gmail.com>
2023-06-10 14:00:32 +0900

    try any

--- src/any.ts
+++ src/any.ts
@@ -6,6 +6,19 @@ let url: string =
   'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
 
 axios.get(url).then(function (response) {
-  let data: any = response.data;
+  interface Article {
+    id: number;
+    title: string;
+    description: string;
+  }
+  let data: Article[];
+  data = response.data;
+  data = [
+    {
+      id: 1,
+      title: 'title',
+      description: 'description',
+    },
+  ];
   console.log(data);
 });
